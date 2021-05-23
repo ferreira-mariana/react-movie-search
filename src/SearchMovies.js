8da04ace0b036bab12217fd1246fe968084b083b
Mariana Ferreira <ferreira_mariana@id.uff.br>
2021-05-23 19:13:14 -0300

    states for input value (query) and api result (movies)

--- src/SearchMovies.js
+++ src/SearchMovies.js
@@ -1,20 +1,22 @@
 import "./SearchMovies.css";
-import React from "react";
+import React, {useState} from "react";
 
 export default function SearchMovies() {
 
+  //states
+  const [query, setQuery] = useState('');
+  const [movies, setMovies] = useState([]);
+
   const searchMovies = async (e) => {
     e.preventDefault(); //prevent posting data in the query params, so do not reload the page
-    console.log('submitting')
-
-    const query = "Hidden Figures";
 
     const url = `https://api.themoviedb.org/3/search/movie?api_key=360856431243f95bd6f5ee6c18caed11&language=en-US&query=${query}&page=1&include_adult=false`;
 
     try {
       const res = await fetch(url)
       const data = await res.json();
-      console.log(data);
+      setMovies(data.results)
+      console.log('movies', movies)
     } catch(err) {
       console.error(err)
     }
@@ -24,7 +26,9 @@ export default function SearchMovies() {
   return (
     <form className="form" onSubmit={searchMovies}>
       <label className="label" htmlFor="query">Movie name</label>
-      <input className="input" type="text" name="query" placeholder="i.e. Hidden Figures" />
+      <input className="input" type="text" name="query" placeholder="i.e. Hidden Figures" 
+        value={query} onChange={(event) => setQuery(event.target.value)}
+      />
       <button className="button">Search</button>
     </form>
   )
